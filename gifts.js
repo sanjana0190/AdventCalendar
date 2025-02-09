33c75eeaa3f49db9223776639d2984f727766a7b
sanjana0190 <sanjanavenkatesh22@gmail.com>
2025-02-09 12:53:57 +0530

    updated the descriptions

--- gifts.js
+++ gifts.js
@@ -2,43 +2,46 @@ export const gifts = [
   {
     id: 1,
     title: "Digital Hug Coupon",
-    description: "",
+    description: "A hug to make your day better!!.",
     gifUrl: "/assets/hug.gif",
   },
   {
     id: 2,
     title: "Virtual Coffee Break",
-    description: "",
+    description:
+      "Wanna have a virtual coffee chat? Talk about how your day's going?",
     gifUrl: "/assets/coffee.gif",
   },
   {
     id: 3,
     title: "Playlist of the Day",
-    description: "",
+    description:
+      "Finding a playlist for that work commute is sooo hard. I made it easy for you. Here's one I think you might like.",
     gifUrl: "/assets/music.gif",
   },
   {
     id: 4,
     title: "Motivational Quote Card",
-    description: "",
+    description: "Be the kind of person your future self will high-five.",
     gifUrl: "/assets/sun.gif",
   },
   {
     id: 5,
     title: "Funny Meme Package",
-    description: "",
+    description:
+      "Let's keep you updated on the current meme knowledge, shall we",
     gifUrl: "/assets/memePackage.gif",
   },
   {
     id: 6,
     title: "Virtual High Five",
-    description: "",
+    description: "Gimme a high-five buddy",
     gifUrl: "/assets/highFive.gif",
   },
   {
     id: 7,
     title: "Digital Flower Bouquet",
-    description: "",
+    description: "Everyone deserves a bouqet. So one bouqet jsut for you!",
     gifUrl: "/assets/bouqet.gif",
   },
   {
@@ -50,7 +53,8 @@ export const gifts = [
   {
     id: 9,
     title: "Movie Night Suggestion",
-    description: "",
+    description:
+      "The Secret Life of Walter Mitty is a must watch movieee! A must!!",
     gifUrl: "/assets/movie.gif",
   },
   {
@@ -62,25 +66,26 @@ export const gifts = [
   {
     id: 11,
     title: "Mini Meditation Guide",
-    description: "",
+    description: "Too much going on? Just follow this meditation guide.",
     gifUrl: "/assets/meditation.gif",
   },
   {
     id: 12,
     title: "Dad Joke of the Day",
-    description: "",
+    description: "Kid: Did you get a haircut? Dad: No, I got them all cut.",
     gifUrl: "/assets/dadJoke.gif",
   },
   {
     id: 13,
     title: "Digital Sticker Pack",
-    description: "",
+    description: "Some stickers for your next chat.",
     gifUrl: "/assets/stars.gif",
   },
   {
     id: 14,
     title: "Song of the Day",
-    description: "",
+    description:
+      "Zou Bisou Bisou                       (just listen to it, trust me)",
     gifUrl: "/assets/song.gif",
   },
   {
@@ -92,7 +97,8 @@ export const gifts = [
   {
     id: 16,
     title: "Mini Digital Game",
-    description: "",
+    description:
+      "I think you should play this game. https://skribbl.io https://makeitmeme.com",
     gifUrl: "/assets/videoGame.gif",
   },
   {
@@ -104,49 +110,53 @@ export const gifts = [
   {
     id: 18,
     title: "Phone Wallpaper",
-    description: "",
+    description: "I think this is a pretty cool wallpaper.",
     gifUrl: "/assets/wallpaper.gif",
   },
   {
     id: 19,
-    title: "Self-Care Tip",
+    title: "Skin-Care Tip",
     description: "",
     gifUrl: "/assets/selfcare.gif",
   },
   {
     id: 20,
     title: "Virtual Dance Break",
-    description: "",
+    description:
+      "Some moves to bust out in your next party. https://www.youtube.com/watch?v=sq0oCPLGWLU",
     gifUrl: "/assets/dance.gif",
   },
   {
     id: 21,
     title: "Digital Pet Cuddles",
-    description: "",
+    description:
+      "Sometimes I just want to squeeeze a puppy. You also want to, don't you? Don't worry I got you.",
     gifUrl: "/assets/petCuddles.gif",
   },
   {
     id: 22,
     title: "Positive News Story",
-    description: "",
+    description: "https://waitbutwhy.com/2015/12/the-tail-end.html",
     gifUrl: "/assets/book.gif",
   },
   {
     id: 23,
-    title: "Holiday Tradition Story",
-    description: "",
+    title: "Story Time",
+    description:
+      "https://podcasts.apple.com/in/podcast/miracle-on-the-l-train/id502304410?i=1000519083173",
     gifUrl: "/assets/story.gif",
   },
   {
     id: 24,
     title: "Christmas Eve Wish",
-    description: "",
+    description:
+      "I wish for you a magical fridge that refills itself with pizza, a bank account that replenished every time you spend and a unicorn that poops glitter.",
     gifUrl: "/assets/christmas.gif",
   },
   {
     id: 25,
     title: "Recipe Card",
-    description: "",
+    description: "/assets/butterChicken.jpg",
     gifUrl: "/assets/recipe.gif",
   },
 ];
