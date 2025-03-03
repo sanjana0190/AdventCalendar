0dfe24e9c77c9b321328be0e5d13c2d8652154e9
sanjana0190 <sanjanavenkatesh22@gmail.com>
2025-03-03 20:58:05 +0530

    updated asset paths for github pages

--- gifts.js
+++ gifts.js
@@ -4,7 +4,7 @@ export const gifts = [
     title: "Day 1",
     giftTitle: "Digital Hug Coupon",
     description: "A hug to make your day better!!",
-    gifUrl: "/assets/hug.gif",
+    gifUrl: "./assets/hug.gif",
   },
   {
     id: 2,
@@ -12,7 +12,7 @@ export const gifts = [
     giftTitle: "Virtual Coffee Break",
     description:
       "Wanna have a virtual coffee chat? Talk about how your day's going?",
-    gifUrl: "/assets/coffee.gif",
+    gifUrl: "./assets/coffee.gif",
   },
   {
     id: 3,
@@ -20,14 +20,14 @@ export const gifts = [
     giftTitle: "Playlist of the Day",
     description:
       "Finding a playlist for that work commute is sooo hard. I made it easy for you. Here's one I think you might like.",
-    gifUrl: "/assets/music.gif",
+    gifUrl: "./assets/music.gif",
   },
   {
     id: 4,
     title: "Day 4",
     giftTitle: "Motivational Quote Card",
     description: "Be the kind of person your future self will high-five.",
-    gifUrl: "/assets/sun.gif",
+    gifUrl: "./assets/sun.gif",
   },
   {
     id: 5,
@@ -35,28 +35,28 @@ export const gifts = [
     giftTitle: "Funny Meme Package",
     description:
       "Let's keep you updated on the current meme knowledge, shall we",
-    gifUrl: "/assets/memePackage.gif",
+    gifUrl: "./assets/memePackage.gif",
   },
   {
     id: 6,
     title: "Day 6",
     giftTitle: "Virtual High Five",
     description: "Gimme a high-five buddy",
-    gifUrl: "/assets/highFive.gif",
+    gifUrl: "./assets/highFive.gif",
   },
   {
     id: 7,
     title: "Day 7",
     giftTitle: "Digital Flower Bouquet",
     description: "Everyone deserves a bouqet. So one bouqet just for you!",
-    gifUrl: "/assets/bouqet.gif",
+    gifUrl: "./assets/bouqet.gif",
   },
   {
     id: 8,
     title: "Day 8",
     giftTitle: "Compliment of the Day",
     description: "You're doing amazing! Keep going!",
-    gifUrl: "/assets/compliment.gif",
+    gifUrl: "./assets/compliment.gif",
   },
   {
     id: 9,
@@ -64,49 +64,49 @@ export const gifts = [
     giftTitle: "Movie Night Suggestion",
     description:
       "The Secret Life of Walter Mitty is a must watch movieee! A must!!",
-    gifUrl: "/assets/movie.gif",
+    gifUrl: "./assets/movie.gif",
   },
   {
     id: 10,
     title: "Day 10",
     giftTitle: "Feel-Good Doodle",
     description: "A little doodle to brighten your day!",
-    gifUrl: "/assets/",
+    gifUrl: "./assets/",
   },
   {
     id: 11,
     title: "Day 11",
     giftTitle: "Mini Meditation Guide",
     description: "Too much going on? Just follow this meditation guide.",
-    gifUrl: "/assets/meditation.gif",
+    gifUrl: "./assets/meditation.gif",
   },
   {
     id: 12,
     title: "Day 12",
     giftTitle: "Dad Joke of the Day",
     description: "Kid: Did you get a haircut? Dad: No, I got them all cut.",
-    gifUrl: "/assets/dadJoke.gif",
+    gifUrl: "./assets/dadJoke.gif",
   },
   {
     id: 13,
     title: "Day 13",
     giftTitle: "Digital Sticker Pack",
     description: "Some stickers for your next chat.",
-    gifUrl: "/assets/stars.gif",
+    gifUrl: "./assets/stars.gif",
   },
   {
     id: 14,
     title: "Day 14",
     giftTitle: "Song of the Day",
     description: "Zou Bisou Bisou (just listen to it, trust me)",
-    gifUrl: "/assets/song.gif",
+    gifUrl: "./assets/song.gif",
   },
   {
     id: 15,
     title: "Day 15",
     giftTitle: "Virtual Fortune Cookie",
     description: "Your future holds many cookies... and good things!",
-    gifUrl: "/assets/fortuneCookie.gif",
+    gifUrl: "./assets/fortuneCookie.gif",
   },
   {
     id: 16,
@@ -114,28 +114,28 @@ export const gifts = [
     giftTitle: "Mini Digital Game",
     description:
       "I think you should play this game: skribbl.io or makeitmeme.com",
-    gifUrl: "/assets/videoGame.gif",
+    gifUrl: "./assets/videoGame.gif",
   },
   {
     id: 17,
     title: "Day 17",
     giftTitle: "Gratitude Prompt",
     description: "What made you smile today?",
-    gifUrl: "/assets/gratitude.gif",
+    gifUrl: "./assets/gratitude.gif",
   },
   {
     id: 18,
     title: "Day 18",
     giftTitle: "Phone Wallpaper",
     description: "I think this is a pretty cool wallpaper.",
-    gifUrl: "/assets/wallpaper.gif",
+    gifUrl: "./assets/wallpaper.gif",
   },
   {
     id: 19,
     title: "Day 19",
     giftTitle: "Self-Care Tip",
     description: "Remember to drink water and get some sunshine today!",
-    gifUrl: "/assets/selfcare.gif",
+    gifUrl: "./assets/selfcare.gif",
   },
   {
     id: 20,
@@ -143,7 +143,7 @@ export const gifts = [
     giftTitle: "Virtual Dance Break",
     description:
       "Some moves to bust out in your next party: youtube.com/watch?v=sq0oCPLGWLU",
-    gifUrl: "/assets/dance.gif",
+    gifUrl: "./assets/dance.gif",
   },
   {
     id: 21,
@@ -151,7 +151,7 @@ export const gifts = [
     giftTitle: "Digital Pet Cuddles",
     description:
       "Sometimes I just want to squeeze a puppy. You also want to, don't you? Don't worry I got you.",
-    gifUrl: "/assets/petCuddles.gif",
+    gifUrl: "./assets/petCuddles.gif",
   },
   {
     id: 22,
@@ -159,7 +159,7 @@ export const gifts = [
     giftTitle: "Positive News Story",
     description:
       "Check out this amazing story: waitbutwhy.com/2015/12/the-tail-end.html",
-    gifUrl: "/assets/book.gif",
+    gifUrl: "./assets/book.gif",
   },
   {
     id: 23,
@@ -167,7 +167,7 @@ export const gifts = [
     giftTitle: "Holiday Tradition Story",
     description:
       "Listen to this heartwarming story: podcasts.apple.com/podcast/miracle-on-the-l-train/id502304410",
-    gifUrl: "/assets/story.gif",
+    gifUrl: "./assets/story.gif",
   },
   {
     id: 24,
@@ -175,13 +175,13 @@ export const gifts = [
     giftTitle: "Christmas Eve Wish",
     description:
       "I wish for you a magical fridge that refills itself with pizza, a bank account that replenishes every time you spend and a unicorn that poops glitter.",
-    gifUrl: "/assets/christmas.gif",
+    gifUrl: "./assets/christmas.gif",
   },
   {
     id: 25,
     title: "Day 25",
     giftTitle: "Recipe Card",
     description: "Here's a delicious recipe for your holiday cooking!",
-    gifUrl: "/assets/recipe.gif",
+    gifUrl: "./assets/recipe.gif",
   },
 ];
